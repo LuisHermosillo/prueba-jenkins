851f936d901bb3d097a5bb3dc750b6c3accd4367
luis.hermosillo <luis.hermosillo@coppel.com>
2024-10-20 18:00:06 -0700

    prueba

---
+++ src/main/java/com/example/pruebajenkins/PruebajenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebajenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebajenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebajenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebajenkins/PruebajenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebajenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebajenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
